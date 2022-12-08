628587ffd8a9e2b46ded44f9122f5fd542e9411e
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2022-12-08 12:20:29 -0300

    Ajustes de imagens

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -1,7 +1,11 @@
+import flower from './img/flower-icon.png'
+
 const Home = () => (
     <div>
         <h1>Sistema de Gerenciamento</h1>
-        <h2>Lumi Enxovais</h2>
+        <h2><img src={flower} alt=""/>
+            Lumi Enxovais
+        <img src={flower} alt=""/></h2>
         <p>by Camila F Barcellos</p>
     </div>
 );
