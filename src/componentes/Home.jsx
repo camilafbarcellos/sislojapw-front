a65248efe03221517aa6e2a5dc32b37aad501fad
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2022-12-08 11:48:03 -0300

    Personalização

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -1,6 +1,8 @@
 const Home = () => (
     <div>
-        <h1>Lumi Enxovais - PWA</h1>
+        <h1>Sistema de Gerenciamento</h1>
+        <h2>Lumi Enxovais</h2>
+        <p>by Camila F Barcellos</p>
     </div>
 );
 
--- src/componentes/Menu.jsx
+++ src/componentes/Menu.jsx
@@ -1,9 +1,12 @@
 import { NavLink } from 'react-router-dom';
+import logo from '/img/logo-lumi.png';
 
 const Menu = () => (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-            <NavLink className="navbar-brand" aria-current="page" exact to="/">LUMI</NavLink>
+            <NavLink className="navbar-brand" aria-current="page" exact to="/">
+                <img src={logo} alt=""/>
+            </NavLink>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
